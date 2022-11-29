f4cdccf46a6161288daf87fb07fa939c3869253f
ximemunoz <92742110+ximemunoz@users.noreply.github.com>
2022-11-29 15:11:19 -0600

    final

--- api/index.js
+++ api/index.js
@@ -1,13 +1,5 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users");
-const postRoute = require("./routes/posts");
-const categoryRoute = require("./routes/categories");
-const multer = require("multer");
-const path = require("path");
 
 dotenv.config();
 app.use(express.json());
